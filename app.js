048921c4870037268655224007fcab4d08972cc7
Areeba Huzaifa <162459943+areeba8820@users.noreply.github.com>
2024-07-15 15:34:11 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// let n = 12;
+// let i = 0;
+// while(i<n){
+//     document.write("<br>" + i)
+//     i++
+// }
+
+let n = 34;
+let i = 12;
+while(i<=n){
+    document.write("<br>" + i)
+    i++
+}
